36c3414c789b20c01292ccec92e88b19800da552
Adnadatt <127004907+Adnadatt@users.noreply.github.com>
2024-07-12 10:44:04 -0600

    Update wave-nao.py

--- wave-nao.py
+++ wave-nao.py
@@ -2,8 +2,6 @@ import time
 import naoqi
 from naoqi import ALProxy
 
-
-
 tts = ALProxy
 
 # text to speech proxy
@@ -19,7 +17,6 @@ posture_proxy = ALProxy("ALRobotPosture",  "10.60.198.90", 9559)
 # Make NAO stand up
 posture_proxy.goToPosture("StandInit", 1.0)
 
-
 while True:
     try:
         with open("C:\\venvProjects\\projectIshani\\flask-server\\responseText.txt", "r") as f:
@@ -30,13 +27,9 @@ while True:
             for line in lines:
                 if line.strip("\n") != text:
                     f.write(line)
-
                 
         time.sleep(1)
     except Exception as e:
         
         print("An error occurred: ", e)
         time.sleep(1)
-
-
-
